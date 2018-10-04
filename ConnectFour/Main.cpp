9d2e2257874ee15e232326a0a81e494e165cfa1b
Haiyoooo <38852079+Haiyoooo@users.noreply.github.com>
2018-10-03 23:49:22 -0700

    Part A
    Part A - Done
    Part B - 70%

--- ConnectFour/Main.cpp
+++ ConnectFour/Main.cpp
@@ -2,15 +2,64 @@ using namespace std; //use standard namespace
 
 #include <iostream> //without this, cout is undefined
 
+void PrintGrid();
+
+//Variables
+const int rows = 6, col = 7;
+char grid[rows][col] = { };
+
 int main()
 {
 	cout << "Welcome to Connect Four\n";
 
-	//creating the grid
-	char grid[7][7] = { "." };
-	cout << grid;
-	cout << "\n";
+		//Create the initial Grid
+		cout << "1234567" << endl;
+		for (int x = 0; x < rows; x++)
+		{
+			for (int y = 0; y < col; y++)
+			{
+				grid[x][y] = '.';
+
+			}
+		}
+
+		PrintGrid();
+
+		// ask player to choose a colmun
+		cout << "Choose a column (1-7):" << endl;
+		int i = 0;
+		cin >> i;
+
+		// check if input is valid
+		if (i >= 1 && i <= col) //VALID
+		{
+			cout << "yay \n";
+			grid[rows-1][i-1] = 'X';
+		}
+		else //INVALID
+		{
+			cout << "boo \n";
+		}
+
+		PrintGrid();
+
 
 	system("pause");
 	return 0;
 }
+
+
+//_________________PRINT A GRID________________
+void PrintGrid()
+{
+	for (int x = 0; x < rows; x++)
+	{
+		for (int y = 0; y < col; y++)
+		{
+			cout << grid [x][y];
+
+		}
+		cout << endl;
+	}
+}
+
